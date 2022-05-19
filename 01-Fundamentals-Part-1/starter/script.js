cb154c5aa156ba78730223da8a7858793ba8a026
UttamkiniH <huttamkini23@gmail.com>
2022-05-19 17:03:42 +0530

    Understanding let,var and const

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let myCountry = "India";
+let myContinent = "Asia";
+let populationCount = "1380.04 million";
+
+// we use let if we want to change the variable
+let age = 20;
+
+
+//we use const so that we dont need to change the variable
+const birthYear = 2000;
+// birthYear = 1998;
+//variable name
+console.log(birthYear);
+console.log(age);
+
